b34dff04799837afe482a0a27c0c73a3e0bc1dca
Daniel Villarroel-Posada <danielv@friends.local>
2020-04-08 22:00:12 -0700

    updating UI and created 2 eventlisteners

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("click", function () {
+  document.getElementById("add-btn").innerHTML = "Hello World!";
+});
+document.addEventListener("click", function () {
+  document.getElementById("search-btn").innerHTML = "Hello World!";
+});
